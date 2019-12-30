ede6ec46975368993d2ebf9b7fd7affcbba03b6e
mohammedalbehairy <mohammedalbehairy90@gmail.com>
2019-12-30 11:17:06 +0200

    change app name

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Gogo
+# portfoliofolio
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.21.
 
--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('gogo app is running!');
+    expect(page.getTitleText()).toEqual('portfoliofolio app is running!');
   });
 
   afterEach(async () => {
