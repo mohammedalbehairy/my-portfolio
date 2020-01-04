5ce15500645c525bf6538c30a54cd5863022f37e
mohammedalbehairy <mohammedalbehairy90@gmail.com>
2020-01-04 20:10:55 +0200

    create admin layout and components

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,3 +1,10 @@
+import { BlogDetailsComponent } from './components/blog-details/blog-details.component';
+import { BlogsComponent } from './components/blogs/blogs.component';
+import { PortfolioDetailsComponent } from './components/portfolio-details/portfolio-details.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { ServicesComponent } from './components/services/services.component';
+import { AboutComponent } from './components/about/about.component';
+import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ContactComponent } from './components/contact/contact.component';
@@ -6,7 +13,35 @@ import { ContactComponent } from './components/contact/contact.component';
 const routes: Routes = [
   {
     path: '',
-    component: ContactComponent
+    component: HomeComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'services',
+    component: ServicesComponent
+  },
+  {
+    path: 'portfolio',
+    component: PortfolioComponent
+  },
+  {
+    path: 'portfolio/:id',
+    component: PortfolioDetailsComponent
+  },
+  {
+    path: 'blogs',
+    component: BlogsComponent
+  },
+  {
+    path: 'blogs/:id',
+    component: BlogDetailsComponent
   },
   {
     path: 'contact',
--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -6,13 +6,27 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from '../auth/login/login.component';
 import { ContactComponent } from './components/contact/contact.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ServicesComponent } from './components/services/services.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { PortfolioDetailsComponent } from './components/portfolio-details/portfolio-details.component';
+import { BlogDetailsComponent } from './components/blog-details/blog-details.component';
+import { BlogsComponent } from './components/blogs/blogs.component';
 
 
 
 @NgModule({
   declarations: [
     AdminLayoutComponent,
-    ContactComponent
+    ContactComponent,
+    HomeComponent,
+    AboutComponent,
+    ServicesComponent,
+    PortfolioComponent,
+    PortfolioDetailsComponent,
+    BlogDetailsComponent,
+    BlogsComponent
   ],
   imports: [
     CommonModule,
